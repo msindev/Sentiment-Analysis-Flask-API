3d01e61734bab8d637d2acebdad6c41d3c240e39
Mohit Singh <mohitwildbeast@gmail.com>
2020-04-06 23:55:09 +0530

    Add sentiment analysis API

---
+++ README.md
@@ -0,0 +1,22 @@
+# Sentiment Analysis Flask API
+This sentiment analysis API allows you to get Sentiment for a sentence using GET and POST methods.
+
+## Requirements
+Use these or any other version if you already have installed.
+    Flask==1.1.1
+    nltk==3.4.5
+## Usage
+Using GET method - 
+
+    http://127.0.0.1:5000?si="Text String to check Sentiment."
+Using POST method - 
+
+    curl http://127.0.0.1:5000 -d "sentiment_input='Text String to check Sentiment.'"
+or use in a web based form and send POST request.
+## Output
+Output is JSON based which is as follows -
+
+    {"sentiment":"Negative"}
+or
+
+    {"sentiment":"Positive"}
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, abort, request, jsonify
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+app = Flask(__name__)
+
+output = {}
+
+def sentiment(sentence):
+
+    nltk.download('vader_lexicon')
+    sid = SentimentIntensityAnalyzer()
+    score = sid.polarity_scores(sentence)['compound']
+    if(score>0):
+        return "Positive"
+    else:
+        return "Negative"
+
+@app.route("/", methods = ["GET","POST"])
+def sentimentRequest():
+    if request.method == "POST":
+        sentence = request.form["sentiment_input"]
+        sent = sentiment(sentence)
+        output['sentiment'] = sent
+        return jsonify(output)
+    else:
+        sentence = request.args.get('si')
+        sent = sentiment(sentence)
+        print(sentence)
+        output['sentiment'] = sent
+        return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
